55e122558b3e5eb3ec2d8795402e09f4777ff1f9
kheeva <oleg.ivliev1@gmail.com>
2018-12-21 15:22:49 +0300

    Delete settings.py

--- lk/authapp/settings.py
+++
@@ -1,9 +0,0 @@
-ourfa_args = ['-x', 'xml',
-              '-c', 'ourfaclient.cfg',
-              '-H', 'billing.captel.ru',
-              '-l', '108_paygate_uniteller',
-              '-P', 'oechLEON756oth7oh',
-              '-S', 'rsa_cert',
-              '-a', 'add_payment',
-              '-turn_on_inet', '1',
-]
