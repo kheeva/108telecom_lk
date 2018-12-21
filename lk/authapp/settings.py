a9b49667047a011827451e769241787c0d789fed
kheeva <oleg.ivliev1@gmail.com>
2018-12-21 15:05:58 +0300

    fix: js/set_balance_color loader via eventlistener. add: promised_pay, notifications, alerts.

---
+++ lk/authapp/settings.py
@@ -0,0 +1,9 @@
+ourfa_args = ['-x', 'xml',
+              '-c', 'ourfaclient.cfg',
+              '-H', 'billing.captel.ru',
+              '-l', '108_paygate_uniteller',
+              '-P', 'oechLEON756oth7oh',
+              '-S', 'rsa_cert',
+              '-a', 'add_payment',
+              '-turn_on_inet', '1',
+]
